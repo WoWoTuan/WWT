f02799dd918ad5dc10211a3f07fa77369ba58760
李用 <liyong@hxxc.com>
2018-04-19 15:48:18 +0800

    first commit

---
+++ src/main/java/com/wwt/WwtApplication.java
@@ -0,0 +1,12 @@
+package com.wwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwtApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wwt/WwtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wwt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
